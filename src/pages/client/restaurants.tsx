111fda30ce82ce394f1bc840768108641f19b69b
park-siwan <siwandevelop@gmail.com>
2023-10-26 03:54:54 -0400

    categories Style

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -38,7 +38,7 @@ const RESTAURANTS_QUERY = gql`
 `;
 
 export const Restaurants = () => {
-  const { data, loading, error } = useQuery<
+  const { data, loading } = useQuery<
     RestaurantsPageQuery,
     RestaurantsPageQueryVariables
   >(RESTAURANTS_QUERY, {
@@ -49,5 +49,32 @@ export const Restaurants = () => {
     },
   });
 
-  return <h1>Restaurants</h1>;
+  return (
+    <div>
+      <form className="flex w-full items-center justify-center bg-gray-800 py-40">
+        <input
+          type="Search"
+          className="input w-3/12 rounded-md border-0"
+          placeholder="Search restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="mx-auto mt-8 max-w-screen-2xl">
+          <div className="mx-auto flex max-w-sm justify-around ">
+            {data?.allCategories.categories?.map((category) => (
+              <div className="flex cursor-pointer flex-col items-center">
+                <div
+                  className="h-14 w-14 rounded-full bg-cover hover:bg-gray-100"
+                  style={{ backgroundImage: `url(${category.coverImg})` }}
+                ></div>
+                <span className="mt-1 text-center text-sm font-medium">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
